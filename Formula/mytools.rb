98763f2b4096fb1be7d138dbff0edb63832d3556
sergey <sergeyro@perun.cloud>
2026-02-01 00:14:18 +0200

    fix sha

--- Formula/mytools.rb
+++ Formula/mytools.rb
@@ -2,7 +2,7 @@ class Mytools < Formula
   desc "My personal development tools installer"
   homepage "https://github.com/sergeyr87/homebrew-tools"
   url "https://github.com/sergeyr87/homebrew-tools/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "74bf4f866544e1b7f8eef7b1ba1029fb5155c11e071f360484f16b1386acec89"
+  sha256 "1f55a3fed05a3d82a8f47e269abc3a049f1e5a9de1cdc74588ff42f21120c69b"
   version "1.0.0"
 
   def install
